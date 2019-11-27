ef234ef2baa38b8823eb8747a61508f27d6fae75
Lucas Bodson <alu0101111254@ull.edu.es>
2019-11-27 16:13:30 +0000

    prueba Plato. porcentaje hidratos de carbono

--- spec/P6_spec.rb
+++ spec/P6_spec.rb
@@ -398,8 +398,11 @@ RSpec.describe P6 do
       expect(@pl1.lipid).to eq(3.1+17.0+6.3)
     end
 
+    it "  Porcentaje de hidrados de carbono del conjunto de alimentos." do
+      expect(@pl1.carbs).to eq(0.0)
+    end
 
-    #Porcentaje de hidrados de carbono del conjunto de alimentos.
+
     #Valor Calórico Total (V.C.T) del conjunto de alimentos expresado en kilocalor´ıas.
     #Se obtiene el plato formateado.
 
